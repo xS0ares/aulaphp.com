d6d72c16131a70f8599d8ad67dac37ffe18b6976
Ediberto Soares <47989722+xS0ares@users.noreply.github.com>
2020-06-06 20:11:59 -0300

    Initial commit

---
+++ parte2_operadoresDoC.php
@@ -0,0 +1,35 @@
+﻿<?php
+    
+    echo '$var = 10 seguido de echo ++$var: ';
+    $var = 10;
+    echo ++$var;
+    
+    echo"<hr>";
+    
+    echo '$var = 10 seguido de echo $var++: ';
+    $var = 10;
+    echo $var++;
+    
+    echo"<br>";
+    
+    echo 'echo $var: ';
+    echo $var;
+    
+    echo"<hr>";
+    
+    echo '$var = 10 seguido de echo --$var: ';
+    $var = 10;
+    echo --$var;
+    
+    echo"<hr>";
+    
+    echo '$var = 10 seguido de echo $var--: ';
+    $var = 10;
+    echo $var--;
+    
+    echo"<br>";
+    
+    echo 'echo $var: ';
+    echo $var;
+    
+    
