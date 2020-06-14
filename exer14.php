53240e3188c8f146dc88e1a50941418f283ee72f
Ediberto Soares <47989722+xS0ares@users.noreply.github.com>
2020-06-13 21:46:35 -0300

    falta a n 10

--- exer14.php
+++ exer14.php
@@ -1,8 +1,8 @@
 <?php
 $num1 = $_POST["num1"];
 $num2 = $_POST["num2"];
-$media =($num1+$num2)/2;
-$metade =($num1-$num2)/2;
+$media = ($num1 + $num2) / 2;
+$metade = ($num1 - $num2) / 2;
 echo "A média artmética é: $media e a metade da diferença entre eles é $metade<br>";
 echo "<br> <hr>";
 $salarioBruto = $_POST["salario"];
@@ -13,7 +13,7 @@ $salariol = $salarioBruto - $desconto;
 echo "Salário Líquido é $salariol";
 echo "<hr>";
 $temp = $_POST["temp"];
-$conversao =5 / 9 * ($temp-32);
+$conversao = 5 / 9 * ($temp - 32);
 $conversao = number_format($conversao, 2, '.', '');
 echo "$temp Graus Fahrenheit  em Graus Celsius é : $conversao<br>";
 echo "<hr>";
@@ -21,9 +21,9 @@ $kmInicial = $_POST["kmInicial"];
 $kmFinal = $_POST["kmFinal"];
 $consumoCombustivel = $_POST["consumoCombustivel"];
 $percurso = $kmFinal - $kmInicial;
-$kmPorLitro = $percurso/$consumoCombustivel;
+$kmPorLitro = $percurso / $consumoCombustivel;
 $kmPorLitro = number_format($kmPorLitro, 2, '.', '');
-$litroPorKm = $consumoCombustivel/$percurso;
+$litroPorKm = $consumoCombustivel / $percurso;
 echo "Quilometragem inicial do veículo: $kmInicial <br>";
 echo "Quilometragem final do veículo: $kmFinal <br>";
 echo "Combustível consumido: $consumoCombustivel  <br>";
@@ -34,23 +34,23 @@ echo "<hr>";
 $alunosMasculino = $_POST["masculino"];
 $alunosFeminino = $_POST["feminino"];
 $aprovados = $_POST["aprovados"];
-$total =($alunosMasculino+$alunosFeminino);
-$pMasculino =($alunosMasculino/$total);
+$total = ($alunosMasculino + $alunosFeminino);
+$pMasculino = ($alunosMasculino / $total);
 $pMasculino = number_format($pMasculino, 2, '.', '');
-$pFeminino =($alunosFeminino/$total);
+$pFeminino = ($alunosFeminino / $total);
 $pFeminino = number_format($pFeminino, 2, '.', '');
-$pAlunosAprovados =($aprovados/$total);
+$pAlunosAprovados = ($aprovados / $total);
 $pAlunosAprovados = number_format($pAlunosAprovados, 2, '.', '');
 echo "O total de alunos é: $total<br> A porcentagem de alunos masculinos é: $pMasculino<br> A porcentagem de alunas femininas é: $pFeminino<br> A porcentagem de alunos aprovados é: $pAlunosAprovados";
 echo "<hr>";
 $qtVit = $_POST["vit"];
 $qtDer = $_POST["der"];
 $qtEmp = $_POST["emp"];
-$quantJogos =($qtVit+$qtDer+$qtEmp);
-$ptV =($qtVit)+3;
-$ptE =($qtDer)+0;
-$ptD =($qtEmp)+1;
-$ptT =$ptV+$ptE+$ptD;
+$quantJogos = ($qtVit + $qtDer + $qtEmp);
+$ptV = ($qtVit) + 3;
+$ptE = ($qtDer) + 0;
+$ptD = ($qtEmp) + 1;
+$ptT = $ptV + $ptE + $ptD;
 echo "A quantidade total de jogos foi: $quantJogos<br> A quantidade de pontos disputados foi:$ptT<br> A quantidade de pontos ganhos:$ptV  <br> A quantidade de pontos perdidos:$ptE ";
 echo "<hr>";
 $salario7 = $_POST["salario7"];
@@ -58,17 +58,17 @@ $sexo = $_POST["sexo"];
 if ($sexo == "Masculino7") {
     $desconto7 = $salario7 * 0.05;
     $SalarioLiquido = $salario7 - $desconto7;
-    echo "Com desconto de 5%, o salário liquido de um funcionário em questão é: $SalarioLiquido.<br>"; 
+    echo "Com desconto de 5%, o salário liquido de um funcionário em questão é: $SalarioLiquido.<br>";
 } else {
     $desconto7 = $salario7 * 0.03;
     $SalarioLiquido = $salario7 - $desconto7;
-    echo "Com desconto de 5%, o salário liquido de um funcionário em questão é: $SalarioLiquido.<br>"; 
+    echo "Com desconto de 5%, o salário liquido de um funcionário em questão é: $SalarioLiquido.<br>";
 };
 echo "<hr>";
 $num8 = $_POST["num8"];
-if ($num8 >0) {
+if ($num8 > 0) {
     echo "O número $num8 é Positivo.<br>";
-} elseif ($num8 <0) {
+} elseif ($num8 < 0) {
     echo "O número $num8 é Negativo.<br>";
 } else {
     echo "O número $num8 é Nulo.<br>";
@@ -77,17 +77,17 @@ echo "<hr>";
 $disciplina = $_POST["disciplina"];
 if ($disciplina == 0) {
     echo "Aprovado! Aproveite as férias !!";
-} elseif ($disciplina <=1 or $disciplina <=3 ) {
+} elseif ($disciplina <= 1 or $disciplina <= 3) {
     echo "Recuperação! Terá que estudar mais!";
 } else {
     echo "Reprovado! Bons estudos no próximo ano!";
 };
 echo "<hr>";
 $data = $_POST["data"];
-$date = new DateTime($data );
-$interval = $date->diff( new DateTime( date('d-m-Y') ) );
-echo $interval->format( '%Y anos' );
-if ($interval == 18 and $genero =="masculino10") {
+$date = new DateTime($data);
+$interval = $date->diff(new DateTime(date('d-m-Y')));
+echo $interval->format('%Y anos');
+if ($interval == 18) {
     echo "Serviço Militar obrigatório.<br>";
 } else {
     echo "Isento de  Serviço Militar.<br>";
